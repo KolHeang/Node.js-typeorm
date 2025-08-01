c79bec30844d533fd3af6b2eabb12e94ab290d3f
KolHeang <kolheang777@gmail.com>
2025-08-01 17:53:27 +0700

    enable 2fa

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import { authController } from "../controllers/auth.controllers";
+import { authMiddleware } from "../middleware/auth.middleware";
 const router = express.Router();
 
 router.post("/register", authController.register as any);
 router.post("/login", authController.login as any);
 router.post("/refresh-token", authController.refreshToken as any);
 router.post("/logout", authController.logout as any);
+router.post("/2fa/enable", authMiddleware, authController.enableTwoFactor as any);
 router.post("/2fa/verify", authController.verifyTwoFactor as any);
 
 export { router as authRoutes };
